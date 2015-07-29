7704e4106860f09625dd61c94fcb561b6cb6e4d1
Ofemino <ofemino@gmail.com>
2015-07-29 11:19:37 +0100

    Changes on PRINTVIEW.aspx
    Modified close button to back and added javascript function to go back
    to previous page.

--- I_LIFE/PRG_LI_REQ_ENTRY_RPT.aspx.vb
+++ I_LIFE/PRG_LI_REQ_ENTRY_RPT.aspx.vb
@@ -3,18 +3,58 @@ Partial Class I_LIFE_PRG_LI_REQ_ENTRY_RPT
     Inherits System.Web.UI.Page
 
     Dim rParams As String() = {"nw", "nw", "new", "new", "new"}
+    Protected FirstMsg As String
 
+    Protected Sub butOK_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles butOK.Click
+        Dim Str() As String
+        If sStartDate.Text <> "" Then
+            Dim ctrlId As Control = FindControl("sStartDate")
+            Str = MOD_GEN.DoDate_Process(sStartDate.Text, ctrlId)
 
-    Protected Sub viewPrintBtn_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles viewPrintBtn.Click
+            If Str(2) = Nothing Then
+                Dim errMsg = Str(0).Insert(18, " Start Date, ")
+                Label1.Text = errMsg.Replace("Javascript:alert('", "").Replace("');", "")
+                FirstMsg = errMsg
+                sStartDate.Focus()
+                Exit Sub
 
-        'rParams(0) = rblTransType.SelectedValue.Trim
-        'rParams(1) = "pStartDate="
-        'rParams(2) = sStartDate.Trim + "&"
-        'rParams(3) = "pEndDate="
-        'rParams(4) = sEndDate.Trim + "&"
+            Else
+                sStartDate.Text = Str(2).ToString()
+            End If
 
+        End If
+
+        If sEndDate.Text <> "" Then
+            Dim ctrlId As Control = FindControl("sEndDate")
+            Str = MOD_GEN.DoDate_Process(sEndDate.Text, ctrlId)
+
+            If Str(2) = Nothing Then
+                Dim errMsg = Str(0).Insert(18, " End Date, ")
+                Label1.Text = errMsg.Replace("Javascript:alert('", "").Replace("');", "")
+                FirstMsg = errMsg
+                sEndDate.Focus()
+                Exit Sub
+
+            Else
+                sEndDate.Text = Str(2).ToString()
+            End If
+
+        End If
+
+        Dim startDate = MOD_GEN.DoConvertToDbDateFormat(sStartDate.Text)
+        Dim endDate = MOD_GEN.DoConvertToDbDateFormat(sEndDate.Text)
+
+        rParams(0) = rblTransType.SelectedValue.Trim
+        rParams(1) = "startDate="
+        rParams(2) = startDate.Trim + "&"
+        rParams(3) = "endDate="
+        rParams(4) = endDate.Trim + "&"
 
         Session("ReportParams") = rParams
-        Response.Redirect("PrintView.aspx")
+        Response.Redirect("~/PrintView.aspx")
+    End Sub
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
     End Sub
 End Class
