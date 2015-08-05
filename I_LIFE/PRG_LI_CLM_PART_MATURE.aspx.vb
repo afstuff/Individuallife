424805215bc63c1811286fb5c2c81855322e8637
Ofemino <ofemino@gmail.com>
2015-08-05 19:48:23 +0100

    recent updates
    recent updates

---
+++ I_LIFE/PRG_LI_CLM_PART_MATURE.aspx.vb
@@ -0,0 +1,46 @@
+﻿
+Partial Class I_LIFE_PRG_LI_CLM_PART_MATURE
+    Inherits System.Web.UI.Page
+    Protected FirstMsg As String
+    Protected PageLinks As String
+
+    Protected STRPAGE_TITLE As String
+    Protected STRMENU_TITLE As String
+    'Protected BufferStr As String
+
+    Protected strStatus As String
+    Protected blnStatus As Boolean
+    Protected blnStatusX As Boolean
+
+    Protected strF_ID As String
+    Protected strQ_ID As String
+    Protected strP_ID As String
+
+    Protected strP_TYPE As String
+    Protected strP_DESC As String
+
+    Protected myTType As String
+
+    Dim strREC_ID As String
+    Protected strOPT As String = "0"
+
+    Protected strTableName As String
+    Dim strTable As String
+    Dim strSQL As String
+
+    Dim strTmp_Value As String = ""
+
+    Dim myarrData() As String
+
+    Dim strErrMsg As String
+
+    Dim basicLc As Decimal
+    Dim basicFc As Decimal
+    Dim addLc As Decimal
+    Dim addFc As Decimal
+    Dim newDateToDb As Date
+
+    Shared _rtnMessage As String
+
+
+End Class
