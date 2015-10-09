268559a798a95248ab011de51ce1ced5832638b0
Adebayo Azeez Adedayo <azeez.adedayo@yahoo.com>
2015-10-09 13:55:47 +0100

    Update on Commission rebate report.

--- I_LIFE/RPT_LI_COMM_REBATE_REPORTS.aspx.vb
+++ I_LIFE/RPT_LI_COMM_REBATE_REPORTS.aspx.vb
@@ -2,19 +2,64 @@
 Imports System.Data.OleDb
 Partial Class I_LIFE_RPT_LI_COMM_REBATE_REPORTS
     Inherits System.Web.UI.Page
-    Dim ErrorInd As String
     Protected FirstMsg As String
     Protected STRMENU_TITLE As String
-    Protected blnStatus As Boolean
-    Protected blnStatusX As Boolean
-    Protected strTableName As String
-    Dim strTable As String
-    Dim strSQL As String
-    Dim rParams As String() = {"nw"}
+    Dim rParams As String() = {"nw", "nw", "new", "new", "new"}
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        ErrorInd = ""
+        
+    End Sub
+
+    Protected Sub butOK_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles butOK.Click
+        Dim str() As String
+        '  Dim reportname As String
+        If (txtStartDate.Text = "") Then
+            Status.Text = "Start date must not be empty"
+            Exit Sub
+        End If
+        If (txtEndDate.Text = "") Then
+            Status.Text = "End date must not be empty"
+            Exit Sub
+        End If
+
+
+
+        str = DoDate_Process(txtStartDate.Text, txtStartDate)
+        If (str(2) = Nothing) Then
+            Dim errMsg = str(0).Insert(18, " Start date, ")
+            Status.Text = errMsg.Replace("Javascript:alert('", "").Replace("');", "")
+            txtStartDate.Focus()
+            Exit Sub
+        Else
+            txtStartDate.Text = str(2).ToString()
+        End If
+
+        str = DoDate_Process(txtEndDate.Text, txtEndDate)
+        If (str(2) = Nothing) Then
+            Dim errMsg = str(0).Insert(18, " End date ")
+            Status.Text = errMsg.Replace("Javascript:alert('", "").Replace("');", "")
+            txtEndDate.Focus()
+            Exit Sub
+        Else
+            txtEndDate.Text = str(2).ToString()
+        End If
+
+        Dim startDate1 = Convert.ToDateTime(DoConvertToDbDateFormat(txtStartDate.Text))
+        Dim endDate1 = Convert.ToDateTime(DoConvertToDbDateFormat(txtEndDate.Text))
+
+        If startDate1 > endDate1 Then
+            Status.Text = "Start Date must not be greater than end date"
+            Exit Sub
+        End If
+
+        Dim startDate = Format(startDate1, "MM/dd/yyyy")
+        Dim endDate = Format(endDate1, "MM/dd/yyyy")
+
         rParams(0) = "rptCommissionRebate1"
+        rParams(1) = "pStartDate="
+        rParams(2) = startDate + "&"
+        rParams(3) = "pEndDate="
+        rParams(4) = endDate + "&"
         Session("ReportParams") = rParams
         Response.Redirect("../PrintView.aspx")
     End Sub
